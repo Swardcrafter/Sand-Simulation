2667815dadd83f5e73cf40c352b0efc9cd3817f7
PlotTwist <84284643+Swardcrafter@users.noreply.github.com>
2024-01-27 14:47:01 -0800

    1

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const canvas = document.querySelector('#main_canvas');
 const ctx = canvas.getContext('2d');
 
+// Config
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
+
+const SIZE = WIDTH / 30;
+
+const COLS = WIDTH / 30;
+const ROWS = HEIGHT / 30;
 
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-ctx.fillRect(30, 30, 5, 5);
+let board = [];
+
+// Generate Empty Board
+for (let i = 0; i < ROWS; i++) {
+	board.push([]);
+	for (let j = 0; j < COLS; j++) {
+		if (Math.random() > 0.5) {
+			board[i].push(0);
+		} else {
+			board[i].push(1);
+		}
+	}
+}
+
+function DrawBoard() {
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+	for (let i = 0; i < ROWS; i++) {
+		for (let j = 0; j < COLS; j++) {
+			let pixel = board[i][j];
+
+			ctx.fillRect(i, j, SIZE, SIZE);
+		}
+	}
+}
+
+DrawBoard();
